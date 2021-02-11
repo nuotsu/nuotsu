28b9ca3bc3161e72cb1b881d12195f6e31b14d3c
Mitchell Christ <xzmaru@gmail.com>
2021-02-10 19:25:35 -0800

    enable gyro

--- src/components/GyroLogo.svelte
+++ src/components/GyroLogo.svelte
@@ -18,18 +18,40 @@ function requestPermission() {
 
 function handleOrientation(event) {
   orientation = event;
-  console.log(event);
 }
 </script>
 
-<h1 class="font-title" on:click|preventDefault={requestPermission}>nuotsu</h1>
+<div class="container" on:click|preventDefault={requestPermission}>
+  <div
+    class="logo font-title"
+    class:isRunning
+    style="
+      --alpha:  {orientation.alpha || 0}deg;
+      --beta:   {orientation.beta || 0}deg;
+      --gamma:  {orientation.gamma || 0};
+    "
+  >nuotsu</div>
+</div>
 
-<div>isRunning: {isRunning}</div>
-
-<div>{JSON.stringify(orientation)}</div>
+<dl>
+  <dt>alpha</dt>  <dd>{orientation.alpha || 0}</dd>
+  <dt>beta</dt>   <dd>{orientation.beta || 0}</dd>
+  <dt>gamma</dt>  <dd>{orientation.gamma || 0}</dd>
+</dl>
 
 <style lang="postcss">
-h1 {
+.container {
+  perspective: 200;
+  text-align: center;
+}
+
+.logo {
   font-size: 4rem;
+  transform:
+    rotate3d(1, calc(var(--gamma) / 15), 0, calc(var(--default, 0) + var(--beta)));
+
+  &.isRunning {
+    --default: -60deg;
+  }
 }
 </style>
